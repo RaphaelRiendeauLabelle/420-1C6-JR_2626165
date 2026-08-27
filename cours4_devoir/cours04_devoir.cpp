cacb4edec7a8b356cf544a65f6e165c142c73706
RaphRiendeau <2626165@cstj.qc.ca>
2026-08-27 13:20:59 -0400

    cours4_devoir

---
+++ cours4_devoir/cours04_devoir.cpp
@@ -0,0 +1,12 @@
+/* Auteur: Raphael Riendeau-Labelle
+Date:20260827
+description : entrées sorties à la console*/
+#include <iostream>
+#include <string>
+#include <format>
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello world!\n";
+}
