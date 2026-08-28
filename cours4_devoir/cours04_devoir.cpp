b3e7542adcc1f1bd5a19e0d5e5f0e7e8f2c10d4a
RaphRiendeau <2626165@cstj.qc.ca>
2026-08-28 09:45:34 -0400

    Cours4_devoir

--- cours4_devoir/cours04_devoir.cpp
+++ cours4_devoir/cours04_devoir.cpp
@@ -13,3 +13,44 @@ int main()
 
 	// Affichage de l'en-tête
 	cout << "--- Cours 04 - devoir ---\n";
+	
+#pragma endregion
+	// *** Exercice.01
+// Description de l’exercice 
+	                                  
+	double a = 10.0 + 12.0 / 8;					// 11.5
+	double b = 27.0 + 3.0 / 14;				    // 27.21 
+	double c = (10 + 20 + 30 + 40 + 50) / 11;	// 13.6363... 
+	double d = (double)c;								// 13 
+	double e = (180.0 / 7.0) + c / 2;					// 32.2142... 
+	double f = d / 15;							// 0.8666... 
+
+
+
+	cout << format("A = {:.2f}\n",a);
+	cout << format("B = {:.5f}\n",b);
+	cout << format("C = {:.3f}\n",c);
+	cout << format("D = {:.0f}\n",d);
+	cout << format("E = {:.8f}\n",e);
+	cout << format("F = {:.4f}\n",f);
+	system("pause");
+	system("cls");
+
+	cout << "Entrez la valeur de A: ";
+	double a1;
+	cin >> a1;
+	cout << "Entrez la valeur de B: ";
+	double b1;
+	cin >> b1;
+	cout << "Entrez la valeur de C: ";
+	double c1;
+	cin >> c1;
+
+	cout << "Resultat de la formule :";
+	
+	
+	
+	
+
+
+} // Fin de la fonction main(), ne pas supprimer
