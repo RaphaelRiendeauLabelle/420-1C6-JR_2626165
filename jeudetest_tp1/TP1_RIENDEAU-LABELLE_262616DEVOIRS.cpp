4d82e9c49283448cc449ae23087edc89ebd64d4b
RaphRiendeau <2626165@cstj.qc.ca>
2026-09-11 09:36:57 -0400

    jeudetest_tp1

--- jeudetest_tp1/TP1_RIENDEAU-LABELLE_262616DEVOIRS.cpp
+++ jeudetest_tp1/TP1_RIENDEAU-LABELLE_262616DEVOIRS.cpp
@@ -27,48 +27,109 @@ int main()
     int numeroClient;
     cin >> numeroClient;
 
-    if (numeroClient <= 5000 && numeroClient >= 3000)
+    // numeroClient = 123456
+
+    if (numeroClient >= 1000000 && numeroClient <= 9999999)
+    {
+        //Type de client Étudiant
+        cout << format("Commande pour l'étudiant {}", numeroClient);
+    }
+    else if (numeroClient >= 10000 && numeroClient <= 20000)
+    {
+        //Type de client Enseignant
+        cout << format("Commande pour l'enseignant {}", numeroClient);
+    }  
+    else if (numeroClient <= 3000 && numeroClient >= 5000)
     {
         //Type de client Administrateur
         cout << format("Commande pour l'administateur {}", numeroClient);
     }
+    else
+    {
+        //erreur1
+        cout << format("Erreur : {} est un matricule ou numéro d’employé invalide, impression annulée.\n", numeroClient);
+        system("pause");
+        return 0;
+    }
+    
+    //NOMBRE DE PAGES 
+    cout << "---Pages---\n";
+    int nombrePages;
+    cin >> nombrePages;
 
-       
-      else if (numeroClient <= 10000 && numeroClient >= 20000)
-        {
-        //Type de client Enseignant
-            cout << format("Commande pour l'enseignant {}", numeroClient);
-        }
+    if (nombrePages >= 0)
+    {
+         cout << format("Le nombre de page sélectionné est {}\n", nombrePages);
+    }
+    else
+    {
+        //erreur2
+        cout << format("Erreur : {} n’est pas un nombre de pages valide, impression annulée.\n", nombrePages);
+    }
 
-        
-      else if (numeroClient <= 100000 && numeroClient >= 9999999)
-        {
-        //Type de client Étudiant
-            cout << format("Commande pour l'étudiant {}", numeroClient);
-        }
-      else
-      {
-          cout << format("Erreur : {} est un matricule ou numéro d’employé invalide, impression annulée.", numeroClient);
-      }
+
     system("pause");
-    system("cls");
-      //NOMBRE DE PAGES 
-      cout << "---Pages---\n";
-      int nombrePages;
-      cin >> nombrePages;
 
-      cout << format("Entrer le nombre de pages :",nombrePages);
 
-    if (nombrePages >=0 && nombrePages <=10000)
-      {
-          cout << format("Erreur : ### n’est pas un nombre de pages valide, impression annulée.");
-      }
+    //Exemplaires
+    cout << "---Exemplaires---\n";
+    int nombreExemplaires;
+    cin >> nombreExemplaires;
 
-      else if (nombrePages <= 0 && nombrePages  >= 10000)
-      {
-        cout << format("Le nombre de page sélectionné est {}", nombrePages)
-      }
-    system("pause");
-    system("cls");
+    // Valider le nombre d'exmplaires (0, -1, -1000)
+    if (nombreExemplaires < 1)
+    {
+        //erreur3
+        cout << format("Erreur : {} n’est pas un nombre d'exemplaire valide, impression annulée.\n", nombreExemplaires);
+        system("pause");
+        return 0;
+    }
+   
+
+    cout << format("Le nombre d'exemplaire sélectionné est {}\n", nombreExemplaires);
+    
+    
+    //Papier
+    cout << "--- Papier ---\n";
+    int nombrePapier;
+    cin >> nombrePapier;
+   
+   // Valider le nombre de papiers
+    if (nombrePapier < 1)
+    {
+        //erreur4
+        cout << format("Erreur : {} n’est pas un nombre de papier valide, impression annulée.\n", nombrePapier);
+        system("pause");
+        return 0;
+    }
+
+   //Sélection du choix de papier
+   
+   //Lettre
+    cout << "--- Papier ---\n";
+
+    //1) Lettre(8 1 / 2 x 11) (###.## $ par page)	// Coût de 0.11 $ par page
+    //2) Légal(8 1 / 2 x 14)  (###.## $ par page)	// Coût de 0.13 $ par page
+    //3) Photo(###.## $ par page)	                // Coût de 1.24 $ par page
+
+    int choixPapier;
+    cin >> choixPapier; // 1 2 3 z
+   
+    // Variable
+    // 0.11 0.13 1.24
+    // prixPagePage
+
+
+    // Vérifier le choix, convertir en une valeur par page (prixPagePage)
+
 #pragma endregion
+#pragma endregion
+
+
+
+
+
+
+    // Calculs
+    // prixPagePage
 }
